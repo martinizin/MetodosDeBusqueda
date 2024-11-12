fc0ba7108bed6ca88d1118959428a3f1c9c1d73f
martinizin <mar45pillacela@gmail.com>
2024-11-12 15:54:51 -0500

    GUI

---
+++ src/BusquedaGUI.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class BusquedaGUI {
+    private JTextField textField1;
+    private JButton AGREGARButton;
+    private JButton BUSCARButton;
+    private JPanel pGeneral;
+
+    public BusquedaGUI() {
+        AGREGARButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        BUSCARButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
