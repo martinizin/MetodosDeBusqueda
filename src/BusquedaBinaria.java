ff06df5dafd3f77a7f1f4177795b520de281a446
Eduardo Andrade <ejac09102006@gmail.com>
2024-11-15 09:58:02 -0500

    Documentacion agregada al metodo

--- src/BusquedaBinaria.java
+++ src/BusquedaBinaria.java
@@ -1,20 +1,20 @@
 public class BusquedaBinaria {
     public static int buscar(int[] array, int valor) {
-        int inicio = 0;
-        int fin = array.length - 1;
+        int inicio = 0; // Índice inicial del arreglo
+        int fin = array.length - 1; //Índice final del arreglo
 
-        while (inicio <= fin) {
-            int medio = inicio + (fin - inicio) / 2;
+        while (inicio <= fin) { //Bucle siempre y cuando se cumpla la condición indicada
+            int medio = inicio + (fin - inicio) / 2; //Calcula el punto medio del rango acutal
 
-            if (array[medio] == valor) {
-                return medio;
+            if (array[medio] == valor) { //Compara el valor calculado en medio con el que se está buscando
+                return medio; //Envia el indice que es el valor encontrado
             } else if (array[medio] < valor) {
-                inicio = medio + 1;
+                inicio = medio + 1; //Sigue buscando hacia la mitad derecha
             } else {
-                fin = medio - 1;
+                fin = medio - 1;//Busca en la mitad de la izquierda
             }
         }
-        return -1;
+        return -1; //Si el valor no se encuentra se retorna -1
     }
 }
 
